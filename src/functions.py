7d59d15f16f2b6afb0614971eb3da887172cda79
skihyeon <hero981001@gmail.com>
2024-04-16 21:36:11 +0900

    feature: data augment

--- src/functions.py
+++ src/functions.py
@@ -2,8 +2,6 @@ import torch
 import os
 import pickle
 import importlib
-from sam import SAM
-
 
 from utils.utils import seed_everything, wandb_login, update_wandb_config
 from optim.trainer import BaseTrainer
@@ -68,7 +66,7 @@ def train_func(run_name, model_name, exp_path,
                                 config.settings['batch_size'], config.settings['shuffle'])
     model = ModelFactory.get_model(model_name, label_encoder).to(device)
     base_optimizer = torch.optim.Adam
-    optimizer = SAM(model.parameters(), base_optimizer, lr=0.1, momentum=0.9)
+    optimizer = SAM(model.parameters(), base_optimizer, lr=config.settings['lr'])
 
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer.base_optimizer, mode='max', factor=0.5, patience=2, threshold_mode='abs', min_lr=1e-8)
 
