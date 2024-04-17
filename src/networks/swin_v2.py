73219c006b9f7ebe8dbf06ae419c1ab9c989a6a1
skihyeon <hero981001@gmail.com>
2024-04-17 13:13:31 +0900

    Implement swin_v2

---
+++ src/networks/swin_v2.py
@@ -0,0 +1,15 @@
+from transformers import Swinv2Model
+import torch.nn as nn
+
+class swin_v2(nn.Module):
+    def __init__(self, le):
+        super().__init__()
+        num_classes = len(le.classes_)
+        self.backbone = Swinv2Model.from_pretrained("microsoft/swinv2-large-patch4-window12to16-192to256-22kto1k-ft")
+        self.classifier = nn.Linear(1000, num_classes)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.classifier(x)
+        return x
+
